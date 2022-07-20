0482970eec119d345fd28384e748c018abba7256
minji <my970524@naver.com>
2022-07-20 15:01:31 +0900

    Add : #1 User 모델링
    - User 모델매니저, 모델 생성
    - admin 페이지 관련 설정(커스텀 유저 모델을 admin 페이지에서 관리할 수 있게)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ("id", "email", "username", "is_admin")
+    list_filter = ("is_admin",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal_info", {"fields": ("username",)}),
+        ("Permissions", {"fields": ("is_admin",)}),
+        ("Datetime_info", {"fields": ("date_joined", "updated_at")}),
+    )
+
+    add_fieldsets = ((None, {"classes": ("wide",), "fields": ("email", "username", "password1", "password2")}),)
+
+    filter_horizontal = ()
+
+    readonly_fields = ["date_joined", "updated_at"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
