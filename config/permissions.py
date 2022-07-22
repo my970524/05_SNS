51b5d27c1e876122c94b8c3f2a0817647868b425
minji <my970524@naver.com>
2022-07-22 23:52:42 +0900

    Chore : Docstring 추가
    - IsOwnerOrReadOnly 클래스에 docstring 추가

--- config/permissions.py
+++ config/permissions.py
@@ -2,6 +2,12 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    """
+    SAFE_METHOD (GET)인 경우 누구나 접근 가능합니다.
+    이외의 http method에서 작성자, 관리자 외에는
+    게시글에 대한 권한이 없습니다.
+    """
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
