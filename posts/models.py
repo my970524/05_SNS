594531460bf6da575b283eca2fee1fbadcf845c5
minji <my970524@naver.com>
2022-07-22 10:07:06 +0900

    Feat : #6 게시글 생성 기능 개발
    - Post 모델 tag 필드 blank=True 설정
    - PostListCreateView 생성
    - 시리얼라이저 생성(TagSerializer, PostListSerializer, PostCreateSerializer)
    - config/urls.py에 path 추가(api/v1/)
    - posts/urls.py에 path 추가(posts)
    - #10 게시글 목록 보기 기능 1차 개발

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ from users.models import User
 
 class Tag(models.Model):
     name = models.CharField("태그이름", max_length=50)
+    # slug = models.SlugField("태그슬러그", max_length=50, unique=True, allow_unicode=True, null=True)
 
     def __str__(self):
         return f"id: {self.pk}/ {self.name}"
@@ -14,14 +15,12 @@ class Post(models.Model):
     title = models.CharField("제목", max_length=200)
     content = models.TextField("내용")
     writer = models.ForeignKey(to=User, verbose_name="작성자", on_delete=models.CASCADE, related_name="posts")
-    tags = models.ManyToManyField(to=Tag, related_name="posts")
+    tags = models.ManyToManyField(to=Tag, blank=True, related_name="posts")
     created_at = models.DateTimeField("작성일자", auto_now_add=True)
     updated_at = models.DateTimeField("수정일자", auto_now=True)
     is_deleted = models.BooleanField("삭제여부", default=False)
     view_counts = models.PositiveIntegerField("조회수", default=0)
-    like_users = models.ManyToManyField(
-        to=User, verbose_name="좋아요한 사람", related_name="like_posts", blank=True, null=True
-    )
+    like_users = models.ManyToManyField(to=User, verbose_name="좋아요한 사람", related_name="like_posts", blank=True)
 
     def __str__(self):
         return f"id: {self.pk}/ title: {self.title}"
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,30 @@
-# from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Post
+from .serializers import PostCreateSerializer, PostListSerializer
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        if self.request.method == "GET":
+            queryset = Post.objects.filter(is_deleted=False)
+        else:
+            queryset = Post.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return PostListSerializer
+        else:
+            return PostCreateSerializer
+
+    def create(self, request):
+        context = {"user": request.user}
+        serializer = self.get_serializer(data=request.data, context=context)
+        print(request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "게시글 작성에 실패했습니다."}, status=status.HTTP_400_BAD_REQUEST)
